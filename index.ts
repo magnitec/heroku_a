d8ade6d375cf75e9883f9e0eff8b6752ad15ed55
magnitec <mgnitecka123@gmail.com>
2021-01-15 19:36:45 +0100

    Add initial files

---
+++ index.ts
@@ -0,0 +1,12 @@
+import http from "http";
+import express from "express";
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world 3");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
