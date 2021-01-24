1dabc47953dd11c5a44fede502dcf392c0573e2e
magnitec <mgnitecka123@gmail.com>
2021-01-24 17:34:46 +0100

    Add static file serving

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import http from "http";
 import express from "express";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.get("/", (req, res) => {
-  // res.send("Hello world 3");
   console.log("main page loaded");
-  res.end("Hello World");
+  // res.end("Hello World");
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
